3e379fa0590e85e4de5bc0e7c83ec74ca4bf39c6
Naimjr10 <njr848262@gmail.com>
2023-09-06 16:05:17 +0700

    Create shared ViewModel

--- app/src/main/java/com/example/cupcake/StartFragment.kt
+++ app/src/main/java/com/example/cupcake/StartFragment.kt
@@ -19,10 +19,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.example.cupcake.databinding.FragmentStartBinding
+import com.example.cupcake.model.OrderViewModel
 
 /**
  * This is the first screen of the Cupcake app. The user can choose how many cupcakes to order.
@@ -33,6 +34,12 @@ class StartFragment : Fragment() {
     // This property is non-null between the onCreateView() and onDestroyView() lifecycle callbacks,
     // when the view hierarchy is attached to the fragment.
     private var binding: FragmentStartBinding? = null
+
+    // Initialize shared ViewModel
+    // shared ViewModel adalah ViewModel yang dipakai oleh fragment-fragment
+    // yang berada dalam satu Activity yang sama
+    // Shared ViewModel dibuat dengan cara delegate property ke activityViewModels()
+    private val sharedViewModel: OrderViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
